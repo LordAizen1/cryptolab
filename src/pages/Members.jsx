f75dcc65b0c851a9410b83ea947047fdf93d034a
Mohammad Kaif <mohdkaif2003@gmail.com>
2025-03-31 20:27:55 +0530

    commented out redundant code

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -129,7 +129,7 @@ export default function Members() {
           ))}
         </div>
 
-        <motion.div
+        {/* <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.6 }}
@@ -146,7 +146,7 @@ export default function Members() {
           >
             View Opportunities
           </motion.button>
-        </motion.div>
+        </motion.div> */}
       </div>
     </motion.div>
   );
